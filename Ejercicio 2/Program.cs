dd9e6b29c36045812b97f571249d02945f4caec1
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:12:46 -0400

    Dibujar Cuadrado 3x3

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DibujarCuadrado3x3();
+        }
+        public static void DibujarCuadrado3x3()
+        {
+            int i;
+            int f;
+            for (i = 0; i <= (3-1); i++)
+            {
+                for (f = 0; f <= (3-1); f++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
